db70645cece984f46c334a296c684f123ee8fb21
NovaTheMachine <kodjmail@gmail.com>
2020-07-18 06:40:45 +0200

    component test

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { findLast } from '@angular/compiler/src/directive_resolver';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'masuper';
+  isAuth = false;
+  
+    
+  
 }
